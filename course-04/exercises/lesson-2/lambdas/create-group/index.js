df539a67a2429031074ad19b3f22efafa07b2b6d
hfurkanvural <hfurkanvural@gmail.com>
2020-05-26 00:19:35 +0300

    create group lambda function added.

---
+++ course-04/exercises/lesson-2/lambdas/create-group/index.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const AWS = require('aws-sdk')
+const uuid = require('uuid')
+
+const docClient = new AWS.DynamoDB.DocumentClient()
+
+const groupsTable = process.env.GROUPS_TABLE
+
+exports.handler = async (event) => {
+    
+   const parsedBody = JSON.parse(event.body);
+
+   const itemId = uuid.v4();
+
+   const newItem = {
+       id : itemId,
+       ...parsedBody
+   }
+    
+   await docClient.put({
+       TableName: groupsTable,
+       Item: newItem
+   }).promise()
+
+    return {
+        statusCode: 201,
+        headers: {
+            'Access-Control-Allow-Origin':'*'
+        },
+        body: JSON.stringify({
+            newItem 
+        }),
+    };
+};
